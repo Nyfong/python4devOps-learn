5d56f9c10a66398da71e72841f8ad62a0bc5ea2c
Nyfong <litongfong12@gmail.com>
2025-07-15 18:23:49 +0700

    reference

---
+++ exception/refrence.py
@@ -0,0 +1,7 @@
+#my refrence
+# This file contains a URL that serves as a reference for understanding exception handling in Python. 
+
+url = "https://www.geeksforgeeks.org/python/python-exception-handling/"
+# This code demonstrates exception handling in Python using try, except, else, and finally blocks.
+
+print(url)
